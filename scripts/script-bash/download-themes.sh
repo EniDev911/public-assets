52e985a4627703ebc73d7f0fc97a8117f795837b
Marco Antonio Contreras <70834807+EniDev911@users.noreply.github.com>
2022-10-07 22:47:31 -0300

    Update download-themes.sh

--- scripts/script-bash/download-themes.sh
+++ scripts/script-bash/download-themes.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/EniDev911/assets/main/scripts/sublime-text/themes/Solarized%20Space%20Predawn.sublime-theme
+# https://raw.githubusercontent.com/EniDev911/assets/main/scripts/Color-scheme/Overlook.tmTheme
+Theme=https://raw.githubusercontent.com/EniDev911/assets/main/scripts/sublime-text/themes/Solarized%20Space%20Predawn.sublime-theme
+ColorScheme=https://raw.githubusercontent.com/EniDev911/assets/main/scripts/Color-scheme/Overlook.tmTheme
+wget $Theme $ColorScheme 
 mkdir -p ~/.config/sublime-text/Packages/User/Themes
+mkdir -p ~/.config/sublime-text/Packages/User/Color-scheme
 mv "Solarized Space Predawn.sublime-theme" ~/.config/sublime-text/Packages/User/Themes
+mv "Overlook.tmTheme" ~/.config/sublime-text/Packages/User/Color-scheme
